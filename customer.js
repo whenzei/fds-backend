96c45e27c4ad1ca28fb701f6a79c72473df9b562
MuhdNurKamal <mnkamal95@gmail.com>
2020-02-13 21:14:11 +0800

    Add db object to routes

--- customer.js
+++ customer.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const db = require('./db')
+const db = require('./db');
 
 router.get("/", (req, res) => {
     db.any('SELECT * FROM Customers', [true])
