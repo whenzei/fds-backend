9f5b1f11fb4a0116395c2df96d3f70b7ee18a5d7
MuhdNurKamal <mnkamal95@gmail.com>
2020-02-13 20:06:19 +0800

    Add morgan logging

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const customer = require('./customer');
 const rider = require('./rider');
 const staff = require('./staff');
 const manager = require('./manager');
+const morgan = require('morgan')
+
 const port = process.env.PORT || "8000";
 
+app.use(morgan('tiny'));
 app.use('/customer', customer);
 app.use('/rider', rider);
 app.use('/staff', staff);
