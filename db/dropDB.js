510ef69590a41e4ac39eaebb82936809af5502ae
MuhdNurKamal <mnkamal95@gmail.com>
2020-03-15 16:19:50 +0800

    Add dropDB

---
+++ db/dropDB.js
@@ -0,0 +1,18 @@
+const initOptions = {}
+const pgp = require('pg-promise')(initOptions);
+const cn = {
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: 'postgres'
+};
+const db = pgp(cn);
+const dbName = 'fds';
+db.none("DROP DATABASE " + dbName)
+    .then(data => {
+        console.log("Dropped DB" + dbName);
+    })
+    .catch(error => {
+        console.log(error);
+    });
