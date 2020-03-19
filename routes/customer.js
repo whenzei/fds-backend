7cedf55c5b1dced4149658e19778e3650b370a54
MuhdNurKamal <mnkamal95@gmail.com>
2020-03-19 14:36:25 +0800

    Add rest controller + endpoints

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const { getRestaurant, getMenu, getAllRestaurants } = require('../controllers/restaurant')
 
 router.get("/", (req, res) => res.send(`Hi I'm ${req.user.name}. I'm a ${req.user.role}.`))
 
+router.get("/restaurants", async (req, res, next) => {
+    let restaurants = []
+    try {
+        restaurants = await getAllRestaurants();
+    } catch (err) {
+        next(err)
+    }
+    return res.send(restaurants);
+})
+
+
+router.get("/restaurants/:rid", async (req, res, next) => {
+    let menu = []
+    try {
+        menu = await getMenu(req.params.rid);
+    } catch (err) {
+        next(err)
+    }
+    return res.send(menu)
+})
 module.exports = router;
