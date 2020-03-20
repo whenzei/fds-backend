184e8437f4a0ab5aa61ea0cde3ef1e477984bccf
MuhdNurKamal <mnkamal95@gmail.com>
2020-03-20 17:50:37 +0800

    Add express validator

--- routes/customer.js
+++ routes/customer.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { getRestaurant, getMenu, getAllRestaurants } = require('../controllers/restaurant')
+const { getMenu, getAllRestaurants } = require('../controllers/restaurant')
+const { check } = require('express-validator');
+const { validate } = require('../validate')
 
 router.get("/", (req, res) => res.send(`Hi I'm ${req.user.name}. I'm a ${req.user.role}.`))
 
@@ -15,13 +17,18 @@ router.get("/restaurants", async (req, res, next) => {
 })
 
 
-router.get("/restaurants/:rid", async (req, res, next) => {
-    let menu = []
-    try {
-        menu = await getMenu(req.params.rid);
-    } catch (err) {
-        return next(err)
-    }
-    return res.send(menu)
-})
+router.get("/restaurants/:rid",
+    [
+        check('rid').isInt()
+    ],
+    validate
+    , async (req, res, next) => {
+        let menu = []
+        try {
+            menu = await getMenu(req.params.rid);
+        } catch (err) {
+            return next(err)
+        }
+        return res.send(menu)
+    })
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -3,14 +3,19 @@ const router = express.Router()
 const passport = require('passport')
 const _ = require('lodash')
 const jwt = require('jsonwebtoken');
-const {JWT_SECRET_KEY} = require("../auth/strategies")
+const { JWT_SECRET_KEY } = require("../auth/strategies")
+const { check } = require('express-validator')
+const { validate } = require('../validate')
 
-router.post('/',
-    passport.authenticate('local', {session: false}),
+router.post('/', [
+    check('username').trim().escape().not().isEmpty(),
+    check('password').trim().escape().not().isEmpty()
+], validate, (req, res, next) => { console.log(req.body.password); next() },
+    passport.authenticate('local', { session: false }),
     async (req, res) => {
         const user = _.omit(req.user, ['password', 'passwordhash', 'salt'])
-        const token = jwt.sign({uid: user.uid}, JWT_SECRET_KEY);
-        return res.json({user, token});
+        const token = jwt.sign({ uid: user.uid }, JWT_SECRET_KEY);
+        return res.json({ user, token });
     }
 );
 module.exports = router
