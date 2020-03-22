11bc4ac39f32d1dc555a6c8e1518603418a4c012
Aditya Agarwal <cooladi1998@gmail.com>
2020-03-22 19:33:59 +0800

    Add ability for manager to add staff and riders

---
+++ routes/signupBusiness.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const {addUser} = require('../controllers/manager')
+
+router.post('/', async (req, res, next) => {
+    try {
+        console.log(req.body.user.role);
+        const result = await addUser(req.body.user);
+    } catch (err) {
+        return next(err)
+    }
+    return res.send(req.body.user.name.toString() + " added successfully.");
+});
+
+
+module.exports = router;
