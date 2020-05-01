c8a2982f6b5ccef811297e141f030f36d22f3406
MuhdNurKamal <mnkamal95@gmail.com>
2020-05-01 12:29:36 +0800

    Update generate orders to latest date

--- db/fillTables.js
+++ db/fillTables.js
@@ -332,7 +332,7 @@ const Frequents = [
     [4, 4, '2020-03-01 19:10:25-07']
 ];
 
-const {Collates, Orders, Reviews, Ratings} = generate_orders_collates_ratings_reviews(Customers, Restaurants, Addresses, Riders, Food, '2019-01-01', '2019-04-30', 2, null)
+const {Collates, Orders, Reviews, Ratings} = generate_orders_collates_ratings_reviews(Customers, Restaurants, Addresses, Riders, Food, '2019-01-01', '2020-04-30', 2, null)
 
 // (shiftid, starttime1, endtime1, starttime2, endtime2)
 const Shifts = [
