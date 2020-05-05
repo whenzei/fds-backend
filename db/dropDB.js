ae81072693a033fea11e6a5a40e16af137939d79
MuhdNurKamal <mnkamal95@gmail.com>
2020-05-05 14:52:40 +0800

    Drop db if exists

--- db/dropDB.js
+++ db/dropDB.js
@@ -1,7 +1,7 @@
 const initOptions = {}
 const posgresDb = require('./posgresDB')
 async function dropDB(dbName) {
-    await posgresDb.none("DROP DATABASE " + dbName)
+    await posgresDb.none("DROP DATABASE IF EXISTS " + dbName)
     console.log(dbName + " dropped")
 }
 
